ee0ff1e55016d9f5377bede6787925226a2726e4
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-06 12:11:49 +0530

    Lenght of the co-ordinates using the Cartesian system

--- lineComparison.java
+++ lineComparison.java
@@ -1,8 +1,25 @@
 package Line_Comparison;
+import java.util.*;
 public class lineComparison 
 {
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Line Comparison Computation Program" );
-    }
+        System.out.println("Enter the first co-ordinate");
+   	    Scanner sc = new Scanner(System.in);
+        int X1=sc.nextInt();
+        int Y1=sc.nextInt();
+        System.out.println("Enter the second co-ordinate");
+        int X2=sc.nextInt();
+        int Y2=sc.nextInt();
+        System.out.println("The first co-ordinate is (" +X1 +","+Y1+") ");
+        System.out.println("The second co-ordinate is (" +X2 +","+Y2+") ");
+        double length2;
+        int p3=(X2-X1)*(X2-X1);
+        int p4=(Y2-Y1)*(Y2-Y1);
+        length2=Math.sqrt(p3+p4);
+        System.out.println("length of the line is "+length2);
+   }
+
 }
+
