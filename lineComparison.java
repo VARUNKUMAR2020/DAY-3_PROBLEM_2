8401773a75c427c1a2edd46e6139e58bda41f45e
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-06 07:13:16 +0530

    Line Comparison

---
+++ lineComparison.java
@@ -0,0 +1,8 @@
+package Line_Comparison;
+public class lineComparison 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation Program" );
+    }
+}
